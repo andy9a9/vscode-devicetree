c359ccda5354c33014efc5eb6dc0ab6fa80762dd
Andrej Valek <andy@skyrain.eu>
2026-01-07 19:29:46 +0100

    fix(diagnostics): remove one additional tab

--- src/test/diagnostics.test.ts
+++ src/test/diagnostics.test.ts
@@ -157,8 +157,8 @@ suite('DTS Diagnostics - Real-world Cases', () => {
     test('Should handle typical pinctrl definition', async () => {
         const input = `pinctrl_spec: specGrp {
 \tfsl,pins =
-\t\t<MX8MP_IOMUXC_SAI2_TXFS__GPIO4_IO24\t\t0x116>,\t/* short comment */
-\t\t<MX8MP_IOMUXC_SAI1_RXC__GPIO4_IO01\t\t0x006>;
+\t\t<MX8MP_IOMUXC_SAI2_TXFS__GPIO4_IO24\t0x116>,\t/* short comment */
+\t\t<MX8MP_IOMUXC_SAI1_RXC__GPIO4_IO01\t0x006>;
 };`;
         const diagnostics = await getDiagnostics(input);
         // Should not warn for reasonably formatted pinctrl
